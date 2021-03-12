8e96cae55dbc49a6f80693501a045fd83b72e8d9
Yougigun <s14031403@gmail.com>
2021-03-12 23:48:19 +0800

    add read body example

---
+++ 027-1_read_body/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	// req.Body is a reader and only read once
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", b)
+	// Does not work because read the body already 
+	v := req.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST">
+	 <input type="text" name="q">
+	 <input type="submit">
+	</form>
+	<br>`+v)
+}
